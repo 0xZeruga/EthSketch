1c6bed82755e2c90e59ba59cd4a02186ec1830ec
Jacob Eriksson <jacobbrickane@gmail.com>
2018-03-04 15:13:14 +0100

    Refactor Pixels
    Removed Pixel contract since it raised the Gas cost to high. Also
    removed functionality for changing Pixel properties outside of Pixel
    scope. Initiated Getters and Setters through 2D-array structs. Will
    investigate how to initiate struct arrays in Solidity.

--- Solidity/EthSketh.sol
+++ Solidity/EthSketh.sol
@@ -3,49 +3,6 @@ pragma solidity ^0.4.19;
 import "./ERC20.sol"; 
 import "./SafeMath.sol";
 
-contract Pixel is IERC20() {
-
-    uint256 public Id;
-    
-    //Color default to white
-    string public Colorhex = "#ffffff";
-    
-    //Inital price
-    uint256 public StartPrice = 5;
-    
-    //Times pixel has been purchased
-    uint256 public TimesPurchased = 0;
-    
-    function Pixel(uint256 id, string colorhex, uint256 startprice, uint256 timespurchased) public {
-        Id = id;
-        Colorhex = colorhex;
-        StartPrice = startprice;
-        TimesPurchased = timespurchased;
-    }
-    
-    using SafeMath for uint256;
-    
-    //Get the Cost
-    function getCost(uint _startprice, uint256 _times) public pure returns(uint cost) {
-        return _startprice*(_times+1);
-    }
-    //The current price of pixel
-    uint256 public cost = getCost(StartPrice,TimesPurchased);
-    
-    //The current pixel owner
-    address public pixelowner;
-
-    //Safely increments counter
-    function addpixelpurchasecounter() public view {
-       TimesPurchased.add(1);
-    }
-    
-    //Returns hex color
-    function setcolorhex(string _hex) public {
-        Colorhex = _hex;
-    }
-}
-
 contract Canvas {
 
     //Address to which the funds will be sent
@@ -58,7 +15,100 @@ contract Canvas {
     
     address[] newContracts;
     
-    //Use these somehow?
+    struct PixelTest {
+        uint256 id;
+        uint256 x;
+        uint256 y;
+        uint256 constant start;
+        bytes32[] color;
+        uint256 count;
+        
+        address owner;
+        
+        //TODO: Find out a way to parse uint256 to bytes32[]
+       // uint256 p = GetPrice(start,count);
+    }
+    
+
+    //Determines max Size in X and Y of the Canvas
+    uint256 constant Size_X = 100;
+    uint256 constant Size_Y = 100;
+    
+    //TODO: Find a way to initiate 2D-array of structs.
+    PixelTest A[Size_X][Size_Y]={0};
+    int i;
+    int j;
+    int x = 0;
+    
+    //Creates inital pixels as structs and sets their default values.
+    for(i=0; i<Size_X;i++) {
+        for(j=0; j<Size_Y; j++) {
+            struct PixelTest A[i][j] t = {x,u,j,5,#FFFFFF,0};
+            t.id = x;
+            x++
+        }
+    }
+    
+    //Get examples
+    
+    //Find Struct by coordinate
+    function GetPixelByCord(uint256 _x, uint256 _y) returns (struct p) {
+        for(int i = 0; i <Size_X; i++) {
+            for(int j = 0; j <Size_Y; j++) {
+                if(A[i][j] == A[_x][_y]) {
+                    return A[i][j];
+                }
+            }
+        }
+    }
+    
+    //Find Struct by id
+    function GetPixelById(uint256 _id) returns (struct p) {
+        for(int i = 0; i <Size_X; i++) {
+            for(int j = 0; j <Size_Y; j++) {
+                if(A[i][j].id == _id) {
+                    return A[i][j];
+                }
+            }
+        }
+    }
+    
+    //Get color
+    function GetColor(uint256 _x, uint256 _y) public view returns (bytes32[] color) {
+        return A[_x,_y].color;
+    }
+    //Get count
+    function GetCount(uint256 _x, uint256 _y) public view returns (uint256 count) {
+        return A[_x,_y].count;
+    }
+    //Get cost
+    function GetPixelPrice(uint256 _x, uint256 _y) public view returns (uint256 cost) {
+        return GetPrice(A[_x][_y].start,A[_x,_y].count);
+    }
+    //Get cost pure
+    function GetPrice(uint256 _start, uint256 _count) pure returns (uint256 _price) {
+        return _start*(_count*1);
+    }
+    
+    
+    //Set examples
+    
+    //Set cost
+    function SetC(uint256 _x, uint256 _y, bytes32[] _c) {
+        A[_x,_y].c = _c;
+    }
+    function SetOwner(address _sender, uint _x, uint _y) {
+        A[_x, _y].owner = _sender;
+    }
+    function IncrementCounter(uint256 _x, uint256 _y) {
+        A[_x,_y].count = A[_x,_y].count.add(1);
+    }
+    function SetHexColor(uint256 _x, uint256 _y, bytes32[] _color) {
+        A[_x,_y].color = _color;
+    }
+    
+    
+    //TODO: Use these somehow?
     //This checks whether Ether has been paid to the address
     function () private payable {
         UpdateColor();
@@ -102,37 +152,6 @@ contract Canvas {
 
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
          return allowed[_owner][_spender];
-    }
-    
-    uint256 constant PIXELAMOUNT = 10000;
-    
-    //Add one empty pixel contract
-    function AddEmptyPixel(uint256 id) public {
-                address newContract =  Pixel(id);
-                //Add contract to list of contracts
-                newContracts.push(newContract);
-    }
-    
-    //Populate pixel contract with PIXELAMOUNT
-    function AddEmptyPixels(uint256 amount) public {
-        for (uint i = 0; i < amount; i++) {
-            AddEmptyPixel(i);
-        }
-    }
-
-    //Set Color to hex at index i
-    function setColor(uint i, string _hex) public {
-        getContract(i).setcolorhex(_hex);
-    }
-    
-    //Get cost of pixel at index i
-    //This should be called from Front once the pixel is hovered and Metamask transaction started.
-    function getCost(uint i) public view returns (uint256 cost) {
-       return getContract(i).cost();
-    }
-    
-    function getContract(uint i) public view returns (Pixel chosenPixel) {
-        return Pixel(newContracts[i]);
     }
     
     //Add initial empty pixels on contract start.
